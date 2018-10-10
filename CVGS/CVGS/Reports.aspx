584afeaeec4d629ef00e763f794937ab87f9874e
Janki Joshi <joshijankii3010@gmail.com>
2018-10-10 13:35:05 -0400

    View Report with Dummy data

---
+++ CVGS/CVGS/Reports.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="Reports" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Reports.aspx.cs" Inherits="CVGS.Reports" %>
+
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <h2><%: Title %>.</h2>
+    <p class="text-danger">
+        <asp:Literal runat="server" ID="ErrorMessage" />
+    </p>
+
+    <div class="form-horizontal">
+        <h4>Create a new account</h4>
+            <asp:DropDownList ID="DdlReports" runat="server">
+                <asp:ListItem Enabled="true" Text="Select Report" Value="-1"></asp:ListItem>
+                <asp:ListItem Text="Game List" Value="GameList" ></asp:ListItem>
+                <asp:ListItem Text="Game Detail" Value="GameDetail"></asp:ListItem>
+                <asp:ListItem Text="Member List" Value="3"></asp:ListItem>
+                <asp:ListItem Text="Member Detail" Value="4"></asp:ListItem>
+                <asp:ListItem Text="Wish List" Value="5"></asp:ListItem>
+                <asp:ListItem Text="Sales Report" Value="6"></asp:ListItem>
+                <asp:ListItem Text="Other Reports" Value="7"></asp:ListItem>
+            </asp:DropDownList>
+        <div class="form-group">
+            <div class="col-md-offset-2 col-md-10">
+                <asp:Button runat="server" OnClick="DownloadReport_Click" Text="Download Report" CssClass="btn btn-default" />
+            </div>
+        </div>
+     </div>
+</asp:Content>
+
---
+++ CVGS/CVGS/Reports.aspx.cs
@@ -0,0 +1,106 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CVGS
+{
+    public partial class Reports : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        protected void DownloadReport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable QueryList = MakeDataTable();
+                String reportName = DdlReports.SelectedItem.Value;
+                //ErrorMessage.Text = reportName;
+                //String url = @"D:\report.pdf";
+                ExportDataTableToPdf(QueryList, @"D:\" + reportName + ".pdf", reportName, this);
+            }
+            catch(Exception ex)
+            {
+                ErrorMessage.Text = ex.Message;
+            }
+        }
+
+        private void ExportDataTableToPdf(DataTable queryList, String url, String v2, Control control)
+        {
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-disposition",
+                "attachment;filename=" + v2 + ".pdf");
+
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Document document = new Document();
+            document.SetPageSize(iTextSharp.text.PageSize.A4);
+            PdfWriter.GetInstance(document, Response.OutputStream);
+            document.Open();
+
+            //Table Data
+            PdfPTable table = new PdfPTable(queryList.Columns.Count);
+            for (int i = 0; i < queryList.Rows.Count; i++)
+            {
+                for (int j = 0; j < queryList.Columns.Count; j++)
+                {
+                    table.AddCell(queryList.Rows[i][j].ToString());
+                }
+            }
+
+            document.Add(table);
+
+            document.Close();
+
+            Response.Write(document);
+
+            Response.End();
+
+            ////MemoryStream ms = new MemoryStream();
+            //FileStream fs = new System.IO.FileStream(url, System.IO.FileMode.Create, System.IO.FileAccess.Write, System.IO.FileShare.None);
+            //Document document = new Document();
+            //document.SetPageSize(iTextSharp.text.PageSize.A4);
+            //PdfWriter writer = PdfWriter.GetInstance(document, fs);
+            //document.Open();
+
+            ////Report Header
+
+            ////Table Data
+            //PdfPTable table = new PdfPTable(queryList.Columns.Count);
+            //for(int i=0; i<queryList.Rows.Count; i++)
+            //{
+            //    for (int j = 0; j< queryList.Columns.Count; j++)
+            //    {
+            //        table.AddCell(queryList.Rows[i][j].ToString());
+            //    }
+            //}          
+        }
+
+        private DataTable MakeDataTable()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("Dosage", typeof(int));
+            table.Columns.Add("Drug", typeof(string));
+            table.Columns.Add("Patient", typeof(string));
+            table.Columns.Add("Date", typeof(DateTime));
+
+            // Here we add five DataRows.
+            table.Rows.Add(25, "Indocin", "David", DateTime.Now);
+            table.Rows.Add(50, "Enebrel", "Sam", DateTime.Now);
+            table.Rows.Add(10, "Hydralazine", "Christoff", DateTime.Now);
+            table.Rows.Add(21, "Combivent", "Janet", DateTime.Now);
+            table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now);
+            return table;
+        }
+    }
+}
