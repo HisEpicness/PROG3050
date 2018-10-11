614e2a381051ef9b8d9997cb752764446daa4b80
Janki Joshi <joshijankii3010@gmail.com>
2018-10-11 19:16:10 -0400

    Changed name to DownloadReports

---
+++ CVGS/CVGS/DownloadReports.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+using System.Data;
+
+namespace CVGS
+{
+    public partial class DownloadReports : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DownloadReport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable QueryList = getTable(DdlReports.SelectedIndex);
+                String reportName = DdlReports.SelectedItem.Value;
+                ExportDataTableToPdf(QueryList, @"D:\" + reportName + ".pdf", reportName, this);
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage.Text = ex.Message;
+            }
+        }
+
+        private DataTable getTable(int selectedIndex)
+        {
+
+        }
+
+        private void ExportDataTableToPdf(DataTable queryList, String url, String v2, Control control)
+        {
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-disposition",
+                "attachment;filename=" + v2 + ".pdf");
+
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Document document = new Document();
+            document.SetPageSize(iTextSharp.text.PageSize.A4);
+            PdfWriter.GetInstance(document, Response.OutputStream);
+            document.Open();
+
+            //Table Data
+            PdfPTable table = new PdfPTable(queryList.Columns.Count);
+            for (int i = 0; i < queryList.Rows.Count; i++)
+            {
+                for (int j = 0; j < queryList.Columns.Count; j++)
+                {
+                    table.AddCell(queryList.Rows[i][j].ToString());
+                }
+            }
+
+            document.Add(table);
+
+            document.Close();
+
+            Response.Write(document);
+
+            Response.End();          
+        }
+
+        private DataTable MakeDataTable()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("Dosage", typeof(int));
+            table.Columns.Add("Drug", typeof(string));
+            table.Columns.Add("Patient", typeof(string));
+            table.Columns.Add("Date", typeof(DateTime));
+
+            // Here we add five DataRows.
+            table.Rows.Add(25, "Indocin", "David", DateTime.Now);
+            table.Rows.Add(50, "Enebrel", "Sam", DateTime.Now);
+            table.Rows.Add(10, "Hydralazine", "Christoff", DateTime.Now);
+            table.Rows.Add(21, "Combivent", "Janet", DateTime.Now);
+            table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now);
+            return table;
+        }
+    }
+}
