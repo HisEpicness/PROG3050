d8643122d12ce95cfd09c9503376a64c76395bac
Janki Joshi <joshijankii3010@gmail.com>
2018-11-29 20:39:16 -0500

    Ratings

--- CVGS/CVGS/Models/ReviewModel.cs
+++ CVGS/CVGS/Models/ReviewModel.cs
@@ -7,9 +7,11 @@ namespace CVGS.Models
 {
     public class ReviewModel
     {
-        //public int id { get; set; }
+        public int id { get; set; }
         public string review { get; set; }
         public decimal gameId { get; set; }
         public string username { get; set; }
+        public bool approve { get; set; }
+        public int rating { get; set; }
     }
 }
